c8be9003d499414073506741670f1afa59a6612d
iamsachin <iamsachin@DESKTOP-8RI6OF2>
2022-07-17 22:03:02 +0530

    added all files

---
+++ src/test/java/practice/SampleContactTest.java
@@ -0,0 +1,20 @@
+package practice;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleContactTest
+{
+WebDriver driver=new ChromeDriver();
+	@Test
+	public void createContact() throws Throwable
+	{
+		String value=System.getProperty("url");
+	    System.getProperty("browser");
+		driver.get(value);
+		System.out.println("contact created");
+	
+
+	}
+}
+//-Durl=http://localhost:8888 -Dbroswer=chrome
---
+++ src/test/java/practice/SampleOrgTest.java
@@ -0,0 +1,19 @@
+package practice;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleOrgTest 
+{
+	WebDriver driver=new ChromeDriver();
+	@Test
+	public void createOrganization() throws Throwable
+	{
+		String value=System.getProperty("url");
+	    System.getProperty("browser");
+		driver.get(value);
+		System.out.println("organization created");
+	
+
+	}
+}
